a293d7391f5ce895f13b09da167d7dc0fdbe83f9
HoseonLee <hoseon310@gmail.com>
2023-10-12 14:13:17 +0900

    D2_1986_지그재그 숫자 수정 직접 데이터를 입력하는 방식에서, ifstream객체를 이용하여 input data를 가져오는 방법으로 바꿈. 앞으로의 풀이에서도 이렇게 할 예정.

--- D2/1986_지그재그 숫자.cpp
+++ D2/1986_지그재그 숫자.cpp
@@ -1,23 +1,36 @@
 #include "pch.h"
 
-int solution(int num)
-{
-	int sum = 0;
-
-	for (int i = 1; i <= num; i++)
-	{
-		if (i % 2 == 1)
-			sum += i;
-		else
-			sum -= i;
-	}
-	return sum;
-}
-
-
 int main()
 {
-	int n;
-	cin >> n;
-	cout << solution(n) << endl;
+	ifstream inputFile;
+	inputFile.open("./input/1986_input.txt");
+
+	if (!inputFile.is_open())
+		cout << "Error! Failed to open the file!" << endl;
+
+	int testCase;
+	int T;
+	inputFile >> T;
+
+	for (testCase = 1; testCase <= T; ++testCase)
+	{
+		int num;
+		inputFile >> num;
+
+		int sum{ 0 };
+		for (int i = 1; i <= num; i++)
+		{
+			if (i % 2 == 1)
+				sum += i;
+			else
+				sum -= i;
+		}
+
+		cout << "#" << testCase << " " << sum << endl;
+
+	}
+
+	inputFile.close();
+
+	return 0;
 }
