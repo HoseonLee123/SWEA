b54a165d569a61c990a7060fc95386d7cdac219d
HoseonLee <hoseon310@gmail.com>
2023-10-13 12:41:05 +0900

    D2_1859_백만 장자  프로젝트 해결, 1. 앞에서부터 탐색 2. 뒤에서부터 탐색 뒤에서부터 탐색하는 방법이 1.1초 정도 더 빠르게 수행됨.

--- D2/1859_백만 장자 프로젝트.cpp
+++ D2/1859_백만 장자 프로젝트.cpp
@@ -15,8 +15,49 @@ int main()
 
 	for (testCase = 1; testCase <= T; ++testCase)
 	{
+		int N;
+		inputFile >> N;
 
+		vector<int> prices(N, 0);
+		for (int i = 0; i < N; i++)
+		{
+			inputFile >> prices[i];
+		}
 
+		// Search from back!
+		long long profit = 0;
+		int maxPrice = 0;
+		for (int i = (prices.size() - 1); i > -1; --i)
+		{
+			if (maxPrice < prices[i])
+				maxPrice = prices[i];
+			else
+				profit += maxPrice - prices[i];
+		}
+		cout << "#" << testCase << " " << profit << endl;
+
+		// Search from front!
+		//long long profit = 0;
+		//int startIndex = 0;
+		//while (startIndex < N)
+		//{
+		//	// Find the maxPriceIndex.
+		//	int maxPriceIndex = startIndex;
+		//	for (int i = startIndex + 1; i < N; i++)
+		//	{
+		//		if (prices[maxPriceIndex] < prices[i])
+		//			maxPriceIndex = i;
+		//	}
+
+		//	// Calculate the profit.
+		//	for (int i = startIndex; i < maxPriceIndex; i++)
+		//	{
+		//		profit += prices[maxPriceIndex] - prices[i];
+		//	}
+
+		//	startIndex = maxPriceIndex + 1;
+		//}
+		//cout << "#" << testCase << " " << profit << endl;
 	}
 
 	return 0;
