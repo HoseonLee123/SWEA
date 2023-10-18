fcac933231dbda9fd3bf779205187849f89fb650
HoseonLee <hoseon310@gmail.com>
2023-10-18 14:05:45 +0900

    D1_2027_대각선 출력하기

---
+++ D1/2027_대각선 출력하기.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (j == i)
+				cout << "#";
+			else
+				cout << "+";
+		}
+		cout << endl;
+	}
+
+
+
+
+	return 0;
+}
